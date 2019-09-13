1f0093a06af65ee998c3c898701dfecd48b9ba57
angeliquemari <angelique@nehmzow.com>
2019-09-12 21:09:01 -0700

    stab at nQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -36,6 +36,7 @@ window.countNRooksSolutions = function(n) { //return the number of valid config 
     if (colIndexOptions === undefined) { //so only build array of options once (like [r,p,s])
       // var colIndexOptions = Array(n).fill(0).map(function (el, index) { return index; });
       var colIndexOptions = Array(n).fill(0).map((el, index) => index); // cleaner version of line above
+
     }
 
     for (var i = 0; i < prevValidCombos.length; i++) {
@@ -56,17 +57,45 @@ window.countNRooksSolutions = function(n) { //return the number of valid config 
     return currColumnIndexCombos;  //all valid combinations
   }; //helper ends here
 
-  var validColumnIndexCombos = getValidColumnIndexCombos(n); // returns array of combination of column indexes of each rook
+  var validColumnIndexCombos = getValidColumnIndexCombos(n); // actually running our function. returns array of combination of column indexes of each rook
   console.log('Number of solutions for ' + n + ' rooks:', validColumnIndexCombos.length);
   return validColumnIndexCombos.length;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  // helper function
+  var getValidBoardMatrix = function (n, board = new Board({n: n}), queenIndex = 0, validConfig = false) {
+    var haveSetQueen = false;
+    for (var i = 0; i < n; i++) { // loop through row indexes
+      // (possible optimization, if queenIndex = 0 i.e. first queen, don't need to check for conflicts)
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+      board.togglePiece(i, queenIndex);
+      if (board.hasAnyQueenConflictsOn(i, queenIndex)) {
+        board.togglePiece(i, queenIndex);
+      } else { // hasAnyQueenConflictsOn is a method of the board class
+        haveSetQueen = true;
+        if (queenIndex === n - 1) {
+          validConfig = true;
+        } else {
+          board = getValidBoardMatrix(n, board, queenIndex + 1);
+        }
+      }
+    }
+    // needs fixing - intended as way to preserve config if we reached end of array, break out of for loops and untoggle past pieces otherwise
+    if (haveSetQueen && !validConfig) {
+      board.togglePiece(i, queenIndex);
+      haveSetQueen = false;
+    } else {
+      break;
+    }
+    return board;
+  };
+  // end of helper function
+
+  var validBoardMatrix = getValidBoardMatrix(n).rows();
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(validBoardMatrix));
+  return validBoardMatrix;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
